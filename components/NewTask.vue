d91ac61648907ee96cd0785313b8ca61f4e9a209
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-03-23 21:42:04 +0200

    feat: create new-task component

---
+++ components/NewTask.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { Task } from "~/types";
+import { nanoid } from "nanoid";
+
+const emit = defineEmits<{ (e: "add", payload: Task): void }>();
+
+const focused = ref(false);
+const title = ref("");
+
+function createTask(e: Event) {
+  if (title.value.trim()) {
+    e.preventDefault();
+    emit("add", {
+      title: title.value.trim(),
+      createdAt: new Date(),
+      id: nanoid(),
+    } as Task);
+  }
+  title.value = "";
+}
+</script>
+
+<template>
+  <div>
+    <textarea
+      v-model="title"
+      @keydown.tab="createTask"
+      @keydown.enter="createTask"
+      class="focus:bg-white bg-transparent focus:shadow focus:p-2 resize-none rounded w-full border-none cursor-pointer"
+      :class="{
+        'h-7': !focused,
+        'h-20': focused,
+      }"
+      style="outline: none !important"
+      @focus="focused = true"
+      @blur="focused = false"
+      :placeholder="!focused ? '+ Add A Card' : 'Enter a title for this card'"
+    ></textarea>
+  </div>
+</template>
